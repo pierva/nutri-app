38710f449a95ce4b19b6345fc63dde1f0d846864
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-11-06 08:10:54 -0500

    feat: Add UserBoard component

---
+++ client/src/components/UserBoard.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+
+import UserService from "../services/user-service"
+
+export default class UserBoard extends Component {
+  state = {
+    content: ''
+  }
+
+  componentDidMount() {
+    UserService.getUserBoard()
+      .then(
+        response => {
+          this.setState(() => ({
+            content: response.data
+          }))
+        }, 
+        error => {
+          this.setState(() => ({
+            content: (error.response && error.response.data) ||
+            error.message ||
+            error.toString()
+          }))
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header className='jumbotron'>
+          <h3>{this.state.content}</h3>
+        </header>
+      </div>
+    )
+  }
+}
